3d985278041657e159394821d4964bd5d11f370a
DivyaRavi92 <100188062+DivyaRavi92@users.noreply.github.com>
2022-06-09 10:48:00 +0530

    Update BatteryMeasurements.c

--- BatteryMeasurements.c
+++ BatteryMeasurements.c
@@ -3,8 +3,46 @@
 
 int getBatteryRange(int *RangeInput, int startRange, int endRange)
 {
-  
-  
+   int arrayLength =0;
+   int valueInRange =0;
+   arrayLength = sizeof(RangeInput);
+   for(int rangeElm=0; rangeElm < arrayLength ; rangeElm++)
+   {
+       checkValueInRange(RangeInput[i],startRange,endRange, &valueInRange))
+      
+   }
+  printf("\n%d-%d,%d",startRange,endRange,valueInRange);
+  return valueInRange;  
 }
 
+#include <stdio.h>
+#include "main.h"
 
+void checkValueInRange(int value,int startRange ,int endRange, int * RangeCheck)
+{
+  if((value >= startRange) && (value <= endRange))
+  {
+    *RangeCheck++;
+  }
+}
+
+int getNoOfRValuesInRange(int* array_p,int startRange ,int endRange)
+{
+  int arrayLength =0;
+  int valueInRange =0;
+  arrayLength = sizeof(array_p);
+  for(int i=0; i< arrayLength ;i++)
+  {
+       if(true == checkInRange(array_p[i],startRange,endRange))
+       {
+         valueInRange ++;
+       }
+       else
+       {
+         
+       }
+  }
+  printf("\n%d-%d,%d",startRange,endRange,valueInRange);
+  return valueInRange;
+}
+        
