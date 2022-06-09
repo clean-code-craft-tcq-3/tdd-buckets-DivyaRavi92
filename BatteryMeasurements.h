be3d8370b63361c9b840c92e0bbb127be2323a74
DivyaRavi92 <100188062+DivyaRavi92@users.noreply.github.com>
2022-06-09 10:54:53 +0530

    Update BatteryMeasurements.h

--- BatteryMeasurements.h
+++ BatteryMeasurements.h
@@ -1,4 +1,4 @@
 #include <stdio.h>
 
 int getBatteryRange(int *RangeInput, int startRange, int endRange);
-void checkValueInRange(int value,int startRange ,int endRange, int * RangeCheck);
+int checkValueInRange(int value,int startRange ,int endRange, int RangeCheck);
