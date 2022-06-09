aa243484b080970776b39510d62241909ca560e4
DivyaRavi92 <100188062+DivyaRavi92@users.noreply.github.com>
2022-06-09 11:00:19 +0530

    Update testBattery.cpp

--- testBattery.cpp
+++ testBattery.cpp
@@ -11,3 +11,19 @@ TEST_CASE("detect set of values  from  specified range within input array of int
    int endBatteryRange = 5;
    REQUIRE(getBatteryRange(sampleRange,startBatteryRange,endBatteryRange) == 2);
 }
+
+//TDD_STEP2: Write the actual definition in .c file but without any content, so test will fail
+TEST_CASE("detect set of values  from  specified range within input array of integers") {
+   int sampleRange[] ={4,5};
+   int startBatteryRange = 4;
+   int endBatteryRange = 5;
+   REQUIRE(getBatteryRange(sampleRange,startBatteryRange,endBatteryRange) == 2);
+}
+
+//TDD_STEP1: Write he actual definition in .c file so the test will pass
+TEST_CASE("detect set of values  from  specified range within input array of integers") {
+   int sampleRange[] ={4,5};
+   int startBatteryRange = 4;
+   int endBatteryRange = 5;
+   REQUIRE(getBatteryRange(sampleRange,startBatteryRange,endBatteryRange) == 2);
+}
