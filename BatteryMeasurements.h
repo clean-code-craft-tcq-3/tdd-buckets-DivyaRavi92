82a136b317d0895992ba583db3a16654c5eebde5
DivyaRavi92 <100188062+DivyaRavi92@users.noreply.github.com>
2022-08-10 13:05:11 +0530

    Update BatteryMeasurements.h

--- BatteryMeasurements.h
+++ BatteryMeasurements.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 void sortArray(int *RangeInput, int length, int startRange, int endRange);
-bool getBatteryRange(int *RangeInput, int length, int startRange, int endRange);
-void printOnConsole(int* readings, int* begin, int* end, int* length); 
+bool getBatteryRange(int *RangeInput, int length);
+void printOnConsole(int startVal, int endVal, int size);   
 void sortedArrayPrintOnConsole(int *RangeInput,int length);
 int isConsecutive(int diff);
