5986f570d1c46a411a5dbc30f6e8bd0d364ab9c7
DivyaRavi92 <100188062+DivyaRavi92@users.noreply.github.com>
2022-08-10 13:05:01 +0530

    Update testBattery.cpp

--- testBattery.cpp
+++ testBattery.cpp
@@ -7,17 +7,13 @@
 //TDD_STEP1: Write only the test code without the actual definition in .c file
 TEST_CASE("test1-no definition") {
    int sampleRange[] ={4,5};
-   int startBatteryRange = 4;
-   int endBatteryRange = 5;
    int arrayLength = sizeof(sampleRange)/ sizeof(sampleRange[0]);
-   REQUIRE(getBatteryRange(sampleRange,arrayLength,startBatteryRange,endBatteryRange) == 1);
+   REQUIRE(getBatteryRange(sampleRange,arrayLength) == 1);
 }
 
 //TDD_STEP2: Write the actual definition in .c file but without any content, so test will fail
 TEST_CASE("test2-test condition fails") {
    int sampleRange[] ={3,3,5,4,10,11,12};
-   int startBatteryRange = 3;
-   int endBatteryRange = 5;
    int arrayLength = sizeof(sampleRange)/ sizeof(sampleRange[0]);
-   REQUIRE(getBatteryRange(sampleRange,arrayLength,startBatteryRange,endBatteryRange) == 1);
+   REQUIRE(getBatteryRange(sampleRange,arrayLength) == 1);
 }
