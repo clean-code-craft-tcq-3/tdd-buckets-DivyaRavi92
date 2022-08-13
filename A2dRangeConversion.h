5d3fef0fdedf7ffea42de6087f6bacafa4dfc203
DivyaRavi92 <100188062+DivyaRavi92@users.noreply.github.com>
2022-08-13 11:26:35 +0530

    Update A2dRangeConversion.h

--- A2dRangeConversion.h
+++ A2dRangeConversion.h
@@ -2,3 +2,5 @@
 #include "math.h"
 
 int convertA2DToAmpereRange(int A2Dvaluearray[], int arraysize, int* Amperearray, int maxAmp, int bitresolution);
+int convertA2DToAmpere(int maxAmp,int A2Dvalue,int bitresolution);
+
