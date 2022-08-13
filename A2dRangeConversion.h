520be619621163951eb26f6282bf26cef2d2c2c4
DivyaRavi92 <100188062+DivyaRavi92@users.noreply.github.com>
2022-08-13 11:39:35 +0530

    Update A2dRangeConversion.h

--- A2dRangeConversion.h
+++ A2dRangeConversion.h
@@ -3,4 +3,5 @@
 
 int convertA2DToAmpereRange(int A2Dvaluearray[], int arraysize, int* Amperearray, int maxAmp, int bitresolution);
 int convertA2DToAmpere(int maxAmp,int A2Dvalue,int bitresolution);
-
+void convertA2DtoAmpere12BitA2D(int maxAmp, int maxA2Dvalue, int *ampereVal);
+void convertA2DtoAmpere10BitA2D(int maxAmp,int maxA2Dvalue, int *ampereVal);
